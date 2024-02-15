494295078d645abed8da52d0650365075faddf2f
mrinalcs <mrinalvbu@gmail.com>
2024-02-15 18:22:19 +0530

    last mod from git

--- _plugins/last_modified_date.rb
+++ _plugins/last_modified_date.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :documents, :post_init do |doc|
   git_dates_log_command = `git log --follow --format=%ad --date=iso-strict -- "#{doc.path}"`
   git_dates = git_dates_log_command.split("\n")
-  doc.data["created_at"] ||= git_dates.first
-  doc.data["last_updated_at"] ||= git_dates.last
+  doc.data["last_modified_at"] ||= git_dates.first
+  doc.data["date"] ||= git_dates.last
 end
