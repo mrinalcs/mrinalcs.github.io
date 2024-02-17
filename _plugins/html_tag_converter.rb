511a404286712a877d3991aab6bacdaf4a09c741
mrinalcs <mrinalvbu@gmail.com>
2024-02-17 23:19:44 +0530

    Add rel="nofollow noopener noreferrer" to anchor tags  and ref to external
    Add rel="nofollow noopener noreferrer" to anchor tags  and ref to external

--- _plugins/html_tag_converter.rb
+++ _plugins/html_tag_converter.rb
@@ -21,8 +21,8 @@
       # Replace a href="/ with a href="https://mrinalcs.github.io/
       content.gsub!('a href="/', 'a href="https://mrinalcs.github.io/')
 
-      # # Update external links
-       content.gsub!(%r{<a\s+(?:[^>]*?\s+)?href="((?!mailto:|tel:|https?://mrinalcs.github.io|http://localhost:4000|/|#)[^"]*)"(?![^>]*?rel=)}, '<a href="\1?ref=mrinalcs.github.io" rel="nofollow noopener noreferrer"')
+      # Add rel="nofollow noopener noreferrer" to anchor tags  and ref to external
+      content.gsub!(%r{<a\s+(?:[^>]*?\s+)?href="((?!mailto:|tel:|https?://mrinalcs.github.io|http://localhost:4000|/|#)[^"]*)"(?![^>]*?rel=)}, '<a href="\1?ref=mrinalcs.github.io" rel="nofollow noopener noreferrer"')
 
       
       # Update the item content
