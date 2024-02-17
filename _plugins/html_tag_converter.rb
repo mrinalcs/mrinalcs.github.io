d6aafddcc03f15734cdefea94a6f7b4dd2d80878
mrinalcs <mrinalvbu@gmail.com>
2024-02-17 18:56:25 +0530

    Update html_tag_converter.rb

--- _plugins/html_tag_converter.rb
+++ _plugins/html_tag_converter.rb
@@ -1,30 +1,28 @@
 [:posts, :pages].each do |hook|
   Jekyll::Hooks.register hook, :post_render do |item|
-    if item.site.config['environment'] == 'production'
-      if item.output_ext == ".html"
-        content = item.output
+    if item.output_ext == ".html"
+      content = item.output
 
-        # Remove HTML comments
-        content.gsub!(/<!--(.*?)-->/m, "")
+      # Remove HTML comments
+      content.gsub!(/<!--(.*?)-->/m, "")
 
-        # Remove meta tag with name="generator"
-        content.gsub!(/<meta\s+name=["']generator["'][^>]*>/i, '')
+      # Remove meta tag with name="generator"
+      content.gsub!(/<meta\s+name=["']generator["'][^>]*>/i, '')
 
-        # Remove trailing spaces before self-closing tag (/>)
-        content.gsub!(/\s\/>/, '>')
+      # Remove trailing spaces before self-closing tag (/>)
+      content.gsub!(/\s\/>/, '>')
 
-        # Remove all empty spaces between HTML tags
-        content.gsub!(/>[[:space:]]+</, '><')
+      # Remove all empty spaces between HTML tags
+      content.gsub!(/>[[:space:]]+</, '><')
 
-        # Replace img src="/ with img src="https://mrinalcs.github.io/
-        content.gsub!('img src="/', 'img src="https://mrinalcs.github.io/')
+      # Replace img src="/ with img src="https://mrinalcs.github.io/
+      content.gsub!('img src="/', 'img src="https://mrinalcs.github.io/')
 
-        # Replace a href="/ with a href="https://mrinalcs.github.io/
-        content.gsub!('a href="/', 'a href="https://mrinalcs.github.io/')
+      # Replace a href="/ with a href="https://mrinalcs.github.io/
+      content.gsub!('a href="/', 'a href="https://mrinalcs.github.io/')
 
-        # Update the item content
-        item.output = content
-      end
+      # Update the item content
+      item.output = content
     end
   end
 end
