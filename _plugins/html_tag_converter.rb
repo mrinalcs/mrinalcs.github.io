475f9560ac0f4aa311dad2abb18ea17cb3f54083
mrinalcs <mrinalvbu@gmail.com>
2024-02-20 17:16:34 +0530

    Update html_tag_converter.rb

--- _plugins/html_tag_converter.rb
+++ _plugins/html_tag_converter.rb
@@ -7,19 +7,19 @@
       content.gsub!(/<!--(.*?)-->/m, "")
 
       # Remove meta tag with name="generator"
-      content.gsub!(/<meta\s+name=["']generator["'][^>]*>/i, '')
+      #content.gsub!(/<meta\s+name=["']generator["'][^>]*>/i, '')
 
       # Remove trailing spaces before self-closing tag (/>)
       content.gsub!(/\s\/>/, '>')
 
       # Remove all empty spaces between HTML tags
-      content.gsub!(/>[[:space:]]+</, '><')
+      #content.gsub!(/>[[:space:]]+</, '><')
 
       # Replace img src="/ with img src="https://mrinalcs.github.io/
-      content.gsub!('img src="/', 'img src="https://mrinalcs.github.io/')
+      #content.gsub!('img src="/', 'img src="https://mrinalcs.github.io/')
 
       # Replace a href="/ with a href="https://mrinalcs.github.io/
-      content.gsub!('a href="/', 'a href="https://mrinalcs.github.io/')
+      #content.gsub!('a href="/', 'a href="https://mrinalcs.github.io/')
 
       # Add rel="nofollow noopener noreferrer" to anchor tags  and ref to external
       content.gsub!(%r{<a\s+href="((?!mailto:|tel:|https?://mrinalcs.github.io|http://localhost:4000|/|#)[^"]*)"(?![^>]*?rel=)}, '<a href="\1?ref=mrinalcs.github.io" target="_blank" rel="nofollow noopener noreferrer"') if content.match?(%r{<a\s+href=})
