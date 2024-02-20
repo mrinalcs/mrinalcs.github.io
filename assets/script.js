08e0f632d3278e4997ff9e08768bf6fd5dfcb321
mrinalcs <mrinalvbu@gmail.com>
2024-02-21 00:32:13 +0530

    updated

--- assets/script.js
+++ assets/script.js
@@ -79,3 +79,78 @@ document.addEventListener('DOMContentLoaded', function() {
         addUnderline(initialSectionId);
     }
 });
+//  external link
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    // HTML modal structure
+    var modalHTML = `
+        <div id="externalLinkModal" class="modal">
+            <div class="modal-content">
+                <h2>External Link</h2>
+                <p id="linkInfo">Do you want to proceed to the external link?</p>
+                <div class="modal-footer">
+                    <button id="cancelBtn" class="button secondary">Cancel</button>
+                    <a id="proceedBtn" class="button" href="#" target="_blank">Proceed</a>
+                </div>
+            </div>
+        </div>
+    `;
+
+    // Append HTML modal structure to body
+    var modalContainer = document.createElement('div');
+    modalContainer.innerHTML = modalHTML;
+    document.body.appendChild(modalContainer);
+
+    // Check for external links and attach event listener
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var href = links[i].getAttribute('href');
+        if (href && !href.startsWith('#') && !href.startsWith('tel:') && !isSameDomain(href)) {
+            links[i].addEventListener('click', function(e){
+                e.preventDefault();
+                var modal = document.getElementById('externalLinkModal');
+                var proceedBtn = document.getElementById('proceedBtn');
+                var cancelBtn = document.getElementById('cancelBtn');
+                var linkTitle = this.getAttribute('title') || this.textContent || this.innerText || 'Untitled';
+                var linkURL = this.getAttribute('href');
+                if (linkURL.startsWith('mailto:')) {
+                    document.getElementById('linkInfo').innerHTML = 'Do you want to send an email to <b>' + linkURL.substring(7) + '</b>  ?';
+                } else {
+                    document.getElementById('linkInfo').innerHTML = 'Do you want to proceed this will take you to <b>' + linkTitle + '</b> (' + linkURL.replace(/[?&]ref=[^&]*/, '') + ') ?';
+                }
+                
+                proceedBtn.href = linkURL;
+
+                modal.style.display = 'block';
+                cancelBtn.addEventListener('click', function(){
+                    modal.style.display = 'none';
+                });
+                window.addEventListener('click', function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = 'none';
+                    }
+                });
+            });
+        }
+    }
+
+    // Function to check if the given URL is from the same domain
+    function isSameDomain(url) {
+        var a = document.createElement('a');
+        a.href = url;
+
+        // Define an array of domains to exclude from comparison
+        var excludedDomains = ['localhost', '127.0.0.1', 'res.cloudinary.com'];
+
+        // Check if the URL hostname is in the excludedDomains array
+        for (var i = 0; i < excludedDomains.length; i++) {
+            if (a.hostname.includes(excludedDomains[i])) {
+                return true;
+            }
+        }
+        
+        return a.hostname === window.location.hostname;
+    }
+});
