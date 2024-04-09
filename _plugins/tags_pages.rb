80c246c0d007f94e271b51baddaa282c79d02d66
mrinalcs <mrinalvbu@gmail.com>
2024-04-09 19:03:34 +0530

    add dates to specefic tags
     date and last_modified_at to each tag page based on the first and last post

--- _plugins/tags_pages.rb
+++ _plugins/tags_pages.rb
@@ -25,13 +25,24 @@ module Jekyll
       @base = base
       @dir  = tag.downcase # Directory name will be the lowercase tag name
       @name = 'index.html'
-
+  
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
       self.data['title'] = "Posts tagged #{tag}"
       self.data['description'] = description if description
       self.data['permalink'] = "/#{tag.downcase}" # Set the permalink without the 'tags/' prefix
+  
+      # Add date and last_modified_at based on the first and last posts with this tag
+      posts_with_tag = site.tags[tag.downcase]
+      if posts_with_tag
+        sorted_posts = posts_with_tag.sort_by { |p| p.date }
+        first_post_date = sorted_posts.first.date
+        last_post_date = sorted_posts.last.date
+        self.data['date'] = first_post_date
+        self.data['last_modified_at'] = last_post_date
+      end
     end
   end
+  
 end
