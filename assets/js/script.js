4f95ad41a73bf360a8145771c314a7e4550126fa
mrinalcs <mrinalvbu@gmail.com>
2024-04-12 10:20:40 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,12 @@ function initMathJax() {
 // Function to initialize PhotoSwipe after each page change
 function initPhotoSwipe() {
     let customOptions = {};
+    const closeSVGString = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 40 40" width="20px" height="20px"><path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"/></svg>';
+
     const options = Object.assign(
         {
+            closeSVG: closeSVGString,
+            zoom: false,
             gallery: "article",
             children: "a.photo-swipe",
             photo_scale: 2,
