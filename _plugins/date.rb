126a821d1684a02a2419cb04f165ffd7924ce3c8
mrinalcs <mrinalvbu@gmail.com>
2024-04-20 11:13:52 +0530

    add commits

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -3,4 +3,8 @@ Jekyll::Hooks.register :documents, :post_init do |doc|
   git_dates = git_dates_log_command.split("\n")
   doc.data["last_modified_at"] ||= git_dates.first
   doc.data["date"] ||= git_dates.last
+
+  git_log_command = `git log --follow --format='%s' -- "#{doc.path}"`
+  commit_messages = git_log_command.split("\n")
+  doc.data["commit_messages"] = commit_messages
 end
