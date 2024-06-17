b51e7a2df3012c8e1e033fda928f26aa122b95ce
mrinalcs <mrinalvbu@gmail.com>
2024-06-17 16:02:18 +0530

    sort_numbers_reverse

--- _plugins/sort_numbers.rb
+++ _plugins/sort_numbers.rb
@@ -1,19 +1,27 @@
-# _plugins/sort_numbers.rb
-
 module Jekyll
-    module SortNumbersFilter
-      def sort_numbers(input)
-        # Ensure input is an array of strings
-        numbers = input.map(&:to_s)
-  
-        # Sort the numbers
-        sorted_numbers = numbers.sort_by(&:to_i)
-  
-        # Return sorted numbers as array of strings
-        sorted_numbers
-      end
+  module SortNumbersFilter
+    def sort_numbers(input)
+      # Ensure input is an array of strings
+      numbers = input.map(&:to_s)
+
+      # Sort the numbers
+      sorted_numbers = numbers.sort_by(&:to_i)
+
+      # Return sorted numbers as array of strings
+      sorted_numbers
+    end
+
+    def sort_numbers_reverse(input)
+      # Ensure input is an array of strings
+      numbers = input.map(&:to_s)
+
+      # Sort the numbers in reverse order
+      sorted_numbers = numbers.sort_by(&:to_i).reverse
+
+      # Return sorted numbers as array of strings
+      sorted_numbers
     end
   end
-  
-  Liquid::Template.register_filter(Jekyll::SortNumbersFilter)
-  
+end
+
+Liquid::Template.register_filter(Jekyll::SortNumbersFilter)
