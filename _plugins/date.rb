35f72e0e42c5eb73b845cfc22c5e0c6a3d924a9c
Mrinal <mrinalvbu@gmail.com>
2024-06-28 10:11:28 +0530

    Check dates
    removed commits messages, apply if not there

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -1,10 +1,9 @@
-Jekyll::Hooks.register :documents, :post_init do |doc|
+[:documents, :pages].each do |hook|
+  Jekyll::Hooks.register hook, :post_init do |doc|
   git_dates_log_command = `git log --follow --format=%ad --date=iso-strict -- "#{doc.path}"`
   git_dates = git_dates_log_command.split("\n")
-  doc.data["last_modified_at"] ||= git_dates.first
-  doc.data["date"] ||= git_dates.last
-
-  git_log_command = `git log --follow --format='%s' -- "#{doc.path}"`
-  commit_messages = git_log_command.split("\n")
-  doc.data["commit_messages"] = commit_messages
+  
+  doc.data["last_modified_at"] ||= git_dates.first unless doc.data.key?("last_modified_at")
+  doc.data["date"] ||= git_dates.last unless doc.data.key?("date")
+end
 end
