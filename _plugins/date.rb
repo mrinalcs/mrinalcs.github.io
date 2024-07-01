3634f037561d51485a963046e42f1a104ec842e2
mrinalcs <mrinalvbu@gmail.com>
2024-07-01 17:03:05 +0530

    check if git dates any --> unless git commit

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -3,7 +3,9 @@
   git_dates_log_command = `git log --follow --format=%ad --date=iso-strict -- "#{doc.path}"`
   git_dates = git_dates_log_command.split("\n")
   
-  doc.data["last_modified_at"] ||= git_dates.first unless doc.data.key?("last_modified_at")
-  doc.data["date"] ||= git_dates.last unless doc.data.key?("date")
-end
+  if git_dates.any?
+    doc.data["last_modified_at"] ||= git_dates.first unless doc.data.key?("last_modified_at")
+    doc.data["date"] ||= git_dates.last unless doc.data.key?("date")
+  end
+ end
 end
